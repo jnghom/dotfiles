4d6e4bab0a53dd1b13e1d3db7cff64259020feea
Jeongho Mok <jngho.m@gmail.com>
2017-05-24 02:57:38 +0900

    7

--- install.sh
+++ install.sh
@@ -3,3 +3,17 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# git-prompt
+if [ ! -e ~/.git-prompt.sh ]; then
+  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+fi
+
+if [ ! -d ~/.bash_it ]; then
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+  ~/.bash_it/install.sh --no-modify-config
+fi
+
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
