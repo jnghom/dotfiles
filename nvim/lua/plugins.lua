87fd2711211f6bb40549e6a505ffac47329b4558
Jeongho Mok <jngho.m@gmail.com>
2021-08-30 21:58:31 +0900

    nvim-cmp

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -280,125 +280,165 @@ return require('packer').startup(function()
     end
   }
 
+  -- use {
+  --   'hrsh7th/vim-vsnip',
+  --   -- https://marketplace.visualstudio.com/search?term=snippets&target=VSCode&category=All%20categories&sortBy=Relevance
+  --   requires = {
+  --     'hrsh7th/vim-vsnip-integ',
+  --     'ylcnfrht/vscode-python-snippet-pack'
+  --   },
+  --   config = function ()
+  --     vim.cmd([[
+  --     " NOTE: You can use other key to expand snippet.
+
+  --     " Expand
+  --     imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+  --     smap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+
+  --     " Expand or jump
+  --     imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
+  --     smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
+
+  --     " Jump forward or backward
+  --     " imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+  --     " smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+  --     " imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+  --     " smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+
+  --     " Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
+  --     " See https://github.com/hrsh7th/vim-vsnip/pull/50
+  --     nmap        s   <Plug>(vsnip-select-text)
+  --     xmap        s   <Plug>(vsnip-select-text)
+  --     nmap        S   <Plug>(vsnip-cut-text)
+  --     xmap        S   <Plug>(vsnip-cut-text)
+
+  --     " If you want to use snippet for multiple filetypes, you can `g:vsnip_filetypes` for it.
+  --     let g:vsnip_filetypes = {}
+  --     let g:vsnip_filetypes.javascriptreact = ['javascript']
+  --     let g:vsnip_filetypes.typescriptreact = ['typescript']
+
+  --     ]])
+  --   end
+  -- }
+  -- use { 'cstrap/python-snippets' }
+
+  -- use {
+  --   'hrsh7th/nvim-compe',
+  --   config = function()
+  --     require'compe'.setup {
+  --       enabled = true;
+  --       autocomplete = true;
+  --       debug = false;
+  --       min_length = 1;
+  --       preselect = 'enable';
+  --       throttle_time = 80;
+  --       source_timeout = 200;
+  --       resolve_timeout = 800;
+  --       incomplete_delay = 400;
+  --       max_abbr_width = 100;
+  --       max_kind_width = 100;
+  --       max_menu_width = 100;
+  --       documentation = {
+  --         border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+  --         winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+  --         max_width = 120,
+  --         min_width = 60,
+  --         max_height = math.floor(vim.o.lines * 0.3),
+  --         min_height = 1,
+  --       },
+  --       source = {
+  --         path = true;
+  --         buffer = true;
+  --         calc = true;
+  --         nvim_lsp = true;
+  --         nvim_lua = true;
+  --         vsnip = false;
+  --         ultisnips = true;
+  --         luasnip = true;
+  --       };
+  --     }
+  --     local t = function(str)
+	      -- return vim.api.nvim_replace_termcodes(str, true, true, true)
+  --     end
+
+  --     local check_back_space = function()
+	      -- local col = vim.fn.col('.') - 1
+	      -- return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+  --     end
+
+  --     -- Use (s-)tab to:
+  --     --- move to prev/next item in completion menuone
+  --     --- jump to prev/next snippet's placeholder
+  --     _G.tab_complete = function()
+	      -- if vim.fn.pumvisible() == 1 then
+		      -- return t "<C-n>"
+	      -- -- elseif vim.fn['vsnip#available'](1) == 1 then
+		      -- -- return t "<Plug>(vsnip-expand-or-jump)"
+	      -- elseif check_back_space() then
+		      -- return t "<Tab>"
+	      -- else
+		      -- return vim.fn['compe#complete']()
+	      -- end
+  --     end
+  --     _G.s_tab_complete = function()
+	      -- if vim.fn.pumvisible() == 1 then
+		      -- return t "<C-p>"
+	      -- -- elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+		      -- -- return t "<Plug>(vsnip-jump-prev)"
+	      -- else
+		      -- -- If <S-Tab> is not working in your terminal, change it to <C-h>
+		      -- return t "<S-Tab>"
+	      -- end
+  --     end
+
+  --     vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+  --     vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+  --     vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+  --     vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+  --     vim.api.nvim_set_keymap('i', '<cr>', 'compe#confirm("<cr>")', { expr = true })
+  --     vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()', { expr = true })
+  --   end
+  -- }
+
+  -- Install nvim-cmp, and buffer source as a dependency
   use {
-    'hrsh7th/vim-vsnip',
-    -- https://marketplace.visualstudio.com/search?term=snippets&target=VSCode&category=All%20categories&sortBy=Relevance
+    "hrsh7th/nvim-cmp",
     requires = {
-      'hrsh7th/vim-vsnip-integ',
-      'ylcnfrht/vscode-python-snippet-pack'
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      -- "hrsh7th/cmp-vsnip",
+      "hrsh7th/cmp-nvim-lua"
     },
-    config = function ()
-      vim.cmd([[
-      " NOTE: You can use other key to expand snippet.
-
-      " Expand
-      imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
-      smap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
-
-      " Expand or jump
-      imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
-      smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
-
-      " Jump forward or backward
-      " imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
-      " smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
-      " imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
-      " smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
-
-      " Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
-      " See https://github.com/hrsh7th/vim-vsnip/pull/50
-      nmap        s   <Plug>(vsnip-select-text)
-      xmap        s   <Plug>(vsnip-select-text)
-      nmap        S   <Plug>(vsnip-cut-text)
-      xmap        S   <Plug>(vsnip-cut-text)
-
-      " If you want to use snippet for multiple filetypes, you can `g:vsnip_filetypes` for it.
-      let g:vsnip_filetypes = {}
-      let g:vsnip_filetypes.javascriptreact = ['javascript']
-      let g:vsnip_filetypes.typescriptreact = ['typescript']
-
-      ]])
-    end
-  }
-  use { 'cstrap/python-snippets' }
-
-  use {
-    'hrsh7th/nvim-compe',
     config = function()
-      require'compe'.setup {
-        enabled = true;
-        autocomplete = true;
-        debug = false;
-        min_length = 1;
-        preselect = 'enable';
-        throttle_time = 80;
-        source_timeout = 200;
-        resolve_timeout = 800;
-        incomplete_delay = 400;
-        max_abbr_width = 100;
-        max_kind_width = 100;
-        max_menu_width = 100;
-        documentation = {
-          border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
-          winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-          max_width = 120,
-          min_width = 60,
-          max_height = math.floor(vim.o.lines * 0.3),
-          min_height = 1,
+      local cmp = require('cmp')
+      cmp.setup {
+        -- You can set mappings if you want
+        mapping = {
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+          ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+          })
         },
-        source = {
-          path = true;
-          buffer = true;
-          calc = true;
-          nvim_lsp = true;
-          nvim_lua = true;
-          vsnip = false;
-          ultisnips = true;
-          luasnip = true;
-        };
+
+        -- You should specify your *installed* sources.
+        sources = {
+          { name = 'nvim_lsp' },
+          -- { name = 'buffer' },
+          { name = 'nvim_lua' },
+          { name = 'path' },
+          -- { name = 'vsnip' },
+        },
       }
-      local t = function(str)
-	      return vim.api.nvim_replace_termcodes(str, true, true, true)
-      end
-
-      local check_back_space = function()
-	      local col = vim.fn.col('.') - 1
-	      return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-      end
-
-      -- Use (s-)tab to:
-      --- move to prev/next item in completion menuone
-      --- jump to prev/next snippet's placeholder
-      _G.tab_complete = function()
-	      if vim.fn.pumvisible() == 1 then
-		      return t "<C-n>"
-	      -- elseif vim.fn['vsnip#available'](1) == 1 then
-		      -- return t "<Plug>(vsnip-expand-or-jump)"
-	      elseif check_back_space() then
-		      return t "<Tab>"
-	      else
-		      return vim.fn['compe#complete']()
-	      end
-      end
-      _G.s_tab_complete = function()
-	      if vim.fn.pumvisible() == 1 then
-		      return t "<C-p>"
-	      -- elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-		      -- return t "<Plug>(vsnip-jump-prev)"
-	      else
-		      -- If <S-Tab> is not working in your terminal, change it to <C-h>
-		      return t "<S-Tab>"
-	      end
-      end
-
-      vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-      vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-      vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-      vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-      vim.api.nvim_set_keymap('i', '<cr>', 'compe#confirm("<cr>")', { expr = true })
-      vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()', { expr = true })
     end
   }
+
 
   use {
     'nvim-telescope/telescope.nvim',
@@ -551,17 +591,21 @@ return require('packer').startup(function()
   -- NvimTreeToggle
   --
 
-  use {
-    'terryma/vim-smooth-scroll',
-    config = function()
-      vim.cmd [[
-      noremap <silent> <c-u> :call smooth_scroll#up(&scroll, 0, 2)<CR>
-      noremap <silent> <c-d> :call smooth_scroll#down(&scroll, 0, 2)<CR>
-      "noremap <silent> <c-b> :call smooth_scroll#up(&scroll*2, 0, 4)<CR>
-      "noremap <silent> <c-f> :call smooth_scroll#down(&scroll*2, 0, 4)<CR>
-      ]]
-    end
-  }
+  -- use {
+  --   'psliwka/vim-smoothie'
+  -- }
+
+  -- use {
+  --   'terryma/vim-smooth-scroll',
+  --   config = function()
+  --     vim.cmd [[
+  --     noremap <silent> <c-u> :call smooth_scroll#up(&scroll, 0, 2)<CR>
+  --     noremap <silent> <c-d> :call smooth_scroll#down(&scroll, 0, 2)<CR>
+  --     "noremap <silent> <c-b> :call smooth_scroll#up(&scroll*2, 0, 4)<CR>
+  --     "noremap <silent> <c-f> :call smooth_scroll#down(&scroll*2, 0, 4)<CR>
+  --     ]]
+  --   end
+  -- }
 
   -- Lua
   use {
@@ -599,6 +643,32 @@ return require('packer').startup(function()
       ]]
     end
   }
+
+  -- packer
+  -- let g:coq_settings = { 'auto_start': v:true } or let g:coq_settings = { 'auto_start': 'shut-up' }
+  -- use {
+  --   'ms-jpq/coq_nvim',
+  --   branch = 'coq',
+  --   require = {{'ms-jpq/coq.artifacts'}, branch = 'artifacts'},
+  --   config = function()
+  --     vim.cmd [[
+  --     " Set recommended to false
+  --     let g:coq_settings = { "keymap.recommended": v:false }
+
+  --     " Keybindings
+  --     ino <silent><expr> <Esc>   pumvisible() ? "\<C-e><Esc>" : "\<Esc>"
+  --     ino <silent><expr> <C-c>   pumvisible() ? "\<C-e><C-c>" : "\<C-c>"
+  --     ino <silent><expr> <BS>    pumvisible() ? "\<C-e><BS>"  : "\<BS>"
+  --     ino <silent><expr> <CR>    pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<CR>"
+  --     ino <silent><expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
+  --     ino <silent><expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<BS>"
+  --     ]]
+  --     require("coq").COQnow("--shut-up")
+  --     -- vim.cmd('COQnow')
+  --   end
+  -- } -- main one
+  -- use { 'ms-jpq/coq.artifacts', branch= 'artifacts'} -- 9000+ Snippets
+
 
 
 
